40255beae480d8212d9b866259f99d1cefa926fd
Carlos Ferreira <carlos@especializati.com.br>
2023-03-07 16:55:13 -0300

    feat: example route laravel

---
+++ app/Http/Controllers/Site/SiteController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+class SiteController
+{
+    public function contact()
+    {
+        return view('site/contact');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,9 @@
 <?php
 
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/contato', [SiteController::class, 'contact']);
 
 Route::get('/', function () {
     return view('welcome');
