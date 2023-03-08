31c1cfb511f0c4514ec27d37c1e56080a33678b9
Carlos Ferreira <carlos@especializati.com.br>
2023-03-08 14:55:51 -0300

    feat: list all supports

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,22 @@
-<h1>Listagem dos suportes</h1>
+<h1>Listagem dos Suportes</h1>
+
+<table>
+    <thead>
+        <th>assunto</th>
+        <th>status</th>
+        <th>descrição</th>
+        <th></th>
+    </thead>
+    <tbody>
+        @foreach($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+                <td>
+                    >
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
