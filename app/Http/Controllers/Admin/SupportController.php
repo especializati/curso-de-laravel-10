ac53e5c555d2f72430870782b0d560e713d62e39
Carlos Ferreira <carlos@especializati.com.br>
2023-03-08 14:46:06 -0300

    feat: create model support, and migrations, and list data

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index()
+    public function index(Support $support)
     {
-        return view('admin/supports/index');
+        $supports = $support->all();
+
+        return view('admin/supports/index', compact('supports'));
     }
 }
---
+++ database/migrations/2023_03_08_143857_create_supports_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supports', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject');
+            $table->enum('status', ['a', 'p', 'c']);
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supports');
+    }
+};
