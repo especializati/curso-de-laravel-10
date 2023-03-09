898de6336835bd4c19a37b601ab9de20c76f099f
Carlos Ferreira <carlos@especializati.com.br>
2023-03-09 15:55:52 -0300

    feat: implement repository support

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -2,23 +2,41 @@
 
 use App\DTO\CreateSupportDTO;
 use App\DTO\UpdateSupportDTO;
+use App\Models\Support;
 use App\Repositories\SupportRepositoryInterface;
 
 class SupportEloquentORM implements SupportRepositoryInterface
 {
+    public function __construct(
+        protected Support $model
+    ) {}
+
     public function getAll(string $filter = null): array
     {
-
+        return $this->model
+                    ->where(function ($query) use ($filter) {
+                        if ($filter) {
+                            $query->where('subject', $filter);
+                            $query->orWhere('body', 'like', "%{$filter}%");
+                        }
+                    })
+                    ->all()
+                    ->toArray();
     }
 
     public function findOne(string $id): stdClass|null
     {
+        $support = $this->model->find($id);
+        if (!$support) {
+            return null;
+        }
 
+        return (object) $support->toArray();
     }
 
     public function delete(string $id): void
     {
-
+        $this->model->findOrFail($id)->delete();
     }
 
     public function new(CreateSupportDTO $dto): stdClass
