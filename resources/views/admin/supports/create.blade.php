b0d61f20df19e539b8339b4247634119193ea44a
Carlos Ferreira <carlos@especializati.com.br>
2023-03-09 16:18:03 -0300

    refactor: use components and reuse forms

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,15 +1,7 @@
 <h1>Nova Dúvida</h1>
 
-@if ($errors->any())
-    @foreach($errors->all() as $error)
-        {{ $error }}
-    @endforeach
-@endif
+<x-alert/>
 
 <form action="{{ route('supports.store') }}" method="POST">
-    {{-- <input type="hidden" value="{{ csrf_token() }}" name="_token"> --}}
-    @csrf()
-    <input type="text" placeholder="Assunto" name="subject" value="{{ old('subject') }}">
-    <textarea name="body" cols="30" rows="5" placeholder="Descrição">{{ old('body') }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.supports.partials.form')
 </form>
