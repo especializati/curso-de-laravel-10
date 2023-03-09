fdc768e450d4277f9edaefce90be1c0496705b12
Carlos Ferreira <carlos@especializati.com.br>
2023-03-09 15:17:01 -0300

    refactor: use dto pattern

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\DTO\CreateSupportDTO;
+use App\DTO\UpdateSupportDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
@@ -40,10 +42,9 @@ class SupportController extends Controller
 
     public function store(StoreUpdateSupport $request, Support $support)
     {
-        $data = $request->validated();
-        $data['status'] = 'a';
-
-        $support->create($data);
+        $this->service->new(
+            CreateSupportDTO::makeFromRequest($request)
+        );
 
         return redirect()->route('supports.index');
     }
@@ -60,15 +61,13 @@ class SupportController extends Controller
 
     public function update(StoreUpdateSupport $request, Support $support, string $id)
     {
-        if (!$support = $support->find($id)) {
+        $support = $this->service->update(
+            UpdateSupportDTO::makeFromRequest($request),
+        );
+
+        if (!$support) {
             return back();
         }
-
-        // $support->subject = $request->subject;
-        // $support->body = $request->body;
-        // $support->save();
-
-        $support->update($request->validated());
 
         return redirect()->route('supports.index');
     }
--- app/Services/SupportService.php
+++ app/Services/SupportService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\DTO\CreateSupportDTO;
+use App\DTO\UpdateSupportDTO;
 use stdClass;
 
 class SupportService
@@ -23,32 +25,14 @@ class SupportService
         return $this->repository->findOne($id);
     }
 
-    public function new(
-        string $subject,
-        string $status,
-        string $body,
-    ): stdClass
+    public function new(CreateSupportDTO $dto): stdClass
     {
-        return $this->repository->new(
-            $subject,
-            $status,
-            $body,
-        );
+        return $this->repository->new($dto);
     }
 
-    public function update(
-        string $id,
-        string $subject,
-        string $status,
-        string $body,
-    ): stdClass|null
+    public function update(UpdateSupportDTO $dto): stdClass|null
     {
-        return $this->repository->update(
-            $id,
-            $subject,
-            $status,
-            $body,
-        );
+        return $this->repository->update($dto);
     }
 
     public function delete(string $id): void
