71922b3a70d8c4fc7d1ab481263e9630b324333c
Carlos Ferreira <carlos@especializati.com.br>
2023-04-13 16:29:42 -0300

    feat: create and use helpers functions

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -13,7 +13,7 @@
         @foreach($supports->items() as $support)
             <tr>
                 <td>{{ $support->subject }}</td>
-                <td>{{ $support->status }}</td>
+                <td>{{ getStatusSupport($support->status) }}</td>
                 <td>{{ $support->body }}</td>
                 <td>
                     <a href="{{ route('supports.show', $support->id) }}">ir</a>
