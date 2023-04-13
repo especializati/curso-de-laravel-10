dc8a3b5b7b53db598f9758e87a7a86bf452af322
Carlos Ferreira <carlos@especializati.com.br>
2023-04-13 16:29:27 -0300

    feat: create enum status supports

--- database/migrations/2023_03_08_143857_create_supports_table.php
+++ database/migrations/2023_03_08_143857_create_supports_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\SupportStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +15,7 @@ return new class extends Migration
         Schema::create('supports', function (Blueprint $table) {
             $table->id();
             $table->string('subject');
-            $table->enum('status', ['a', 'p', 'c']);
+            $table->enum('status', array_column(SupportStatus::cases(), 'name'));
             $table->text('body');
             $table->timestamps();
         });
