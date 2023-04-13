2ae5959cd84aebd501e54b6eb2f259e03ace7166
Carlos Ferreira <carlos@especializati.com.br>
2023-04-13 16:10:30 -0300

    fix: fixed name method get on first page

--- app/Repositories/PaginationPresenter.php
+++ app/Repositories/PaginationPresenter.php
@@ -34,7 +34,7 @@ class PaginationPresenter implements PaginationInterface
 
     public function isFirstPage(): bool
     {
-        return $this->paginator->orFirstPage();
+        return $this->paginator->onFirstPage();
     }
 
     public function isLastPage(): bool
