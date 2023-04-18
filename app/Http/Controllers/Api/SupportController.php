06b8367e4225bff16712479c7c1b2308226b7f14
Carlos Ferreira <carlos@especializati.com.br>
2023-04-18 14:34:35 -0300

    feat: api rest - show and delete

--- app/Http/Controllers/Api/SupportController.php
+++ app/Http/Controllers/Api/SupportController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\StoreUpdateSupport;
 use App\Http\Resources\SupportResource;
 use App\Services\SupportService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class SupportController extends Controller
 {
@@ -40,7 +41,13 @@ class SupportController extends Controller
      */
     public function show(string $id)
     {
-        //
+        if (!$support = $this->service->findOne($id)) {
+            return response()->json([
+                'error' => 'Not Found'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        return new SupportResource($support);
     }
 
     /**
@@ -56,6 +63,14 @@ class SupportController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        if (!$this->service->findOne($id)) {
+            return response()->json([
+                'error' => 'Not Found'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $this->service->delete($id);
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
