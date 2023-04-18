f2feba15dc2d1d5f153447b153e1387ec549d843
Carlos Ferreira <carlos@especializati.com.br>
2023-04-18 14:55:20 -0300

    fix: get id by name "id" or "support"

--- app/DTO/Supports/UpdateSupportDTO.php
+++ app/DTO/Supports/UpdateSupportDTO.php
@@ -14,10 +14,10 @@ class UpdateSupportDTO
         public string $body,
     ) {}
 
-    public static function makeFromRequest(StoreUpdateSupport $request): self
+    public static function makeFromRequest(StoreUpdateSupport $request, string $id = null): self
     {
         return new self(
-            $request->id,
+            $id ?? $request->id,
             $request->subject,
             SupportStatus::A,
             $request->body
