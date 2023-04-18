1e1d6b439c4249ad785d004a246aa667e4829fb0
Carlos Ferreira <carlos@especializati.com.br>
2023-04-18 15:23:25 -0300

    feat: create api adapter

--- app/Http/Controllers/Api/SupportController.php
+++ app/Http/Controllers/Api/SupportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Adapters\ApiAdapter;
 use App\DTO\Supports\CreateSupportDTO;
 use App\DTO\Supports\UpdateSupportDTO;
 use App\Http\Controllers\Controller;
@@ -29,17 +30,7 @@ class SupportController extends Controller
             filter: $request->filter,
         );
 
-        return SupportResource::collection($supports->items())
-                                ->additional([
-                                    'meta' => [
-                                        'total' => $supports->total(),
-                                        'is_first_page' => $supports->isFirstPage(),
-                                        'is_last_page' => $supports->isLastPage(),
-                                        'current_page' => $supports->currentPage(),
-                                        'next_page' => $supports->getNumberNextPage(),
-                                        'previous_page' => $supports->getNumberPreviousPage(),
-                                    ]
-                                ]);
+        return ApiAdapter::toJson($supports);
     }
 
     /**
