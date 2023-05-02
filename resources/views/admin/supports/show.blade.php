a5712128dd52ae0441f18bff3fa68355a5593217
Carlos Ferreira <carlos@especializati.com.br>
2023-05-02 15:57:03 -0300

    style: use tailwind css

--- resources/views/admin/supports/show.blade.php
+++ resources/views/admin/supports/show.blade.php
@@ -1,5 +1,12 @@
-<h1>Detalhes da dúvida {{ $support->id }}</h1>
+@extends('admin.layouts.app')
 
+@section('title', "Detalhes da Dúvida {$support->subject}")
+
+@section('header')
+<h1 class="text-lg text-black-500">Dúvida {{ $support->subject }}</h1>
+@endsection
+
+@section('content')
 <ul>
     <li>Assunto: {{ $support->subject }}</li>
     <li>Status: {{ $support->status }}</li>
@@ -9,5 +16,7 @@
 <form action="{{ route('supports.destroy', $support->id) }}" method="post">
     @csrf()
     @method('DELETE')
-    <button type="submit">Deletar</button>
+    <button type="submit" class="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded">Deletar</button>
 </form>
+
+@endsection
