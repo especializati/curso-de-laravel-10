5bcc1eef9c023a21ed97f2fbb20a26964c8d4a73
Carlos Ferreira <carlos@especializati.com.br>
2023-05-02 14:54:56 -0300

    feat: create and use template

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,4 +1,12 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Fórum')
+
+@section('header')
 <h1>Listagem dos Suportes</h1>
+@endsection
+
+@section('content')
 
 <a href="{{ route('supports.create') }}">Criar Dúvida</a>
 
@@ -27,3 +35,5 @@
 <x-pagination
     :paginator="$supports"
     :appends="$filters" />
+
+@endsection
