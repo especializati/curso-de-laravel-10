bea4bfea1044b12bd23b733fa11fdff9799ebffd
Carlos Ferreira <carlos@especializati.com.br>
2023-06-26 17:03:03 -0300

    feat: create accessor for date

--- app/Models/ReplySupport.php
+++ app/Models/ReplySupport.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -18,6 +20,13 @@ class ReplySupport extends Model
         'support_id',
         'content',
     ];
+
+    public function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $createdAt) => Carbon::make($createdAt)->format('d/m/Y H:i'),
+        );
+    }
 
     public function user(): BelongsTo
     {
