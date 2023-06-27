640a749046b8403f11d47eda136db84f4ba1d3da
Carlos Ferreira <carlos@especializati.com.br>
2023-06-27 15:52:44 -0300

    feat: create helper get initials of name

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -3,7 +3,25 @@
 use App\Enums\SupportStatus;
 
 if (!function_exists('getStatusSupport')) {
-    function getStatusSupport(string $status): string {
+    function getStatusSupport(string $status): string
+    {
         return SupportStatus::fromValue($status);
+    }
+}
+
+if (!function_exists('getInitials')) {
+    function getInitials($name)
+    {
+        $words = explode(' ', $name); // Split the name into an array of words
+        $initials = '';
+
+        foreach ($words as $word) {
+            $initials .= strtoupper(substr($word, 0, 1)); // Append the first character of each word
+            if (strlen($initials) >= 2) {
+                break; // Stop appending initials once we reach the limit
+            }
+        }
+
+        return $initials;
     }
 }
