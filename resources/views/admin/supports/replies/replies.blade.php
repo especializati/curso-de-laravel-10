91dfe7e1a303a63655cb9fff02654d58c3a9c677
Carlos Ferreira <carlos@especializati.com.br>
2023-06-27 14:45:11 -0300

    feat: use gate owner

--- resources/views/admin/supports/replies/replies.blade.php
+++ resources/views/admin/supports/replies/replies.blade.php
@@ -8,11 +8,13 @@
     <div class="md:w-3/5 w-3/4 px-10 flex flex-col gap-2 p-5">
         <div class="flex justify-between">
             <h1 class="text-lg">Detalhes da Dúvida <b>{{ $support->subject }}</b></h1>
-            <form action="{{ route('supports.destroy', $support->id) }}" method="post">
-                @csrf()
-                @method('DELETE')
-                <button type="submit" class="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded">Deletar</button>
-            </form>
+            @can('owner', $support->user['id'])
+                <form action="{{ route('supports.destroy', $support->id) }}" method="post">
+                    @csrf()
+                    @method('DELETE')
+                    <button type="submit" class="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded">Deletar</button>
+                </form>
+            @endcan
         </div>
         <ul>
             <li>Status: <x-status-support :status="$support->status"/></li>
@@ -38,11 +40,15 @@
 
                     <div class="flex justify-between">
                         <span>{{ $reply['created_at'] }}</span>
-                        <form action="{{ route('replies.destroy', [$support->id, $reply['id']]) }}" method="post">
-                            @csrf()
-                            @method('DELETE')
-                            <button type="submit" class="bg-red-500 hover:bg-red-400 text-white py-1 px-4 border-b-4 border-red-700 hover:border-red-500 rounded">Deletar</button>
-                        </form>
+                        @can('owner', $reply['user']['id'])
+                            <form action="{{ route('replies.destroy', [$support->id, $reply['id']]) }}" method="post">
+                                @csrf()
+                                @method('DELETE')
+                                <button type="submit" class="bg-red-500 hover:bg-red-400 text-white py-1 px-4 border-b-4 border-red-700 hover:border-red-500 rounded">Deletar</button>
+                            </form>
+                        @else
+                        --
+                        @endcan
                     </div>
                 </div>
             @empty
