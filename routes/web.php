c8d08f3e34a2ca84a2e2c4b11a2a054a9d5c1e92
Gagema <gabrielgmazolli@gmail.com>
2023-11-11 03:36:17 -0300

    correção de rotas

--- routes/web.php
+++ routes/web.php
@@ -64,26 +64,5 @@ Route::put('/festa/novosconvidadosoperacional1/{id}', [ConvidadosController::cla
 Route::put('/opiniao/{id}',[OpinioesController::class,'novaopiniao'])->name('opiniao.index');
 Route::post('/novaopiniao',[OpinioesController::class,'novaopiniaopost'])->name('novaopiniao.index');
 Route::post('/datasoperacional',[ReservasController::class,'datasoperacional'])->name('datasoperacional.index');
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::post('/supports/{id}/replies', [ReplySupportController::class, 'store'])->name('replies.store');
-    Route::delete('/supports/{id}/replies/{reply}', [ReplySupportController::class, 'destroy'])->name('replies.destroy');
-    Route::get('/supports/{id}/replies', [ReplySupportController::class, 'index'])->name('replies.index');
-
-    // Route::resource('/supports', SupportController::class);
-    Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
-    Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
-    Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
-    Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-    Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
-    Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
-});
-
-require __DIR__ . '/auth.php';
